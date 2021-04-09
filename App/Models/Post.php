a75f9bf325272415c4180ad62a8c90f45877d440
Cdric-art <cdric.art@gmail.com>
2021-04-09 17:18:27 +0200

    Feat: Models for posts and tags

---
+++ App/Models/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use DateTime;
+
+class Post extends Model
+{
+
+    protected string $table = 'posts';
+
+    public function getCreatedAt(): string
+    {
+        return (new DateTime($this->created_at))->format('d/m/Y à H:m');
+    }
+
+    public function getExcerpt(): string
+    {
+        return substr($this->content, 0, 125) . '...';
+    }
+
+    public function getButton(): string
+    {
+        $html = <<<HTML
+        <a rel="stylesheet" href="http://localhost/site_php/posts/$this->id" class="btn btn-primary">Lien vers l'article</a>
+        HTML;
+
+        return $html;
+    }
+
+    public function getTags()
+    {
+        return $this->query("
+            SELECT t.* FROM tags t
+            INNER JOIN post_tag pt on t.id = pt.tag_id
+            WHERE pt.post_id = ?
+            ", $this->id);
+    }
+}
---
+++ App/Models/Tags.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Models;
+
+
+class Tags extends Model
+{
+    protected string $table = 'tags';
+
+    public function getPosts(): array
+    {
+        return $this->query("
+            SELECT p.* FROM posts p
+            INNER JOIN post_tag pt on p.id = pt.post_id
+            WHERE pt.tag_id = ?
+        ", $this->id);
+    }
+}
