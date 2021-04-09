0487c76efdc3a0ef4e51880ce707be461cb3614d
Cdric-art <cdric.art@gmail.com>
2021-04-09 17:17:50 +0200

    Feat: Router

---
+++ Routes/Router.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Router;
+
+use App\Exceptions\NotFoundException;
+
+class Router
+{
+
+    public string $url;
+    public array $routes = [];
+
+    public function __construct(string $url)
+    {
+        $this->url = trim($url, '/');
+    }
+
+    public function getUrl(string $path, string $action): void
+    {
+        $this->routes['GET'][] = new Route($path, $action);
+    }
+
+    public function run()
+    {
+        foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route) {
+            if ($route->matches($this->url)) {
+                return $route->execute();
+            }
+        }
+
+        throw new NotFoundException();
+    }
+}
